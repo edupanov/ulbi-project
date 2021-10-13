a959eba5e1ba77edf09f500e37bbbdb29b2cdc7c
Egor <edupanov@gmail.com>
2021-10-13 10:52:45 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      this is work
-    </div>
-  );
+    return (
+        <div className="App">
+            hi
+        </div>
+    );
 }
 
 export default App;
